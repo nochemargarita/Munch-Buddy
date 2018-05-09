3906ae7b0284f51a53b467ad6d7df9c0c25cc9d2
Margarita Noche <Margarita@users-MBP.localdomain>
2018-05-08 17:23:47 -0700

    Created a function to open file and just get all categories of restaurants.

--- seed.py
+++ seed.py
@@ -34,10 +34,37 @@ def to_text_file():
 
     file.close()
 
+def get_categories(filename):
+    """Get all restaurant categories from a text file."""
+
+    categories = []
+    with open(filename) as filename:
+        
+        for i in filename:
+           for x in range(len(json.loads(i))):
+                categories.append(json.loads(i)[x]['categories'])
+
+    return categories
+
+def add_categories_to_db():
+    """Add all categories alias to database."""
+
+    alias_categories = []
+    for i in get_categories('restaurants.txt'):
+        for x in range(len(i)):
+             if i[x]['alias'] not in alias_categories:
+                alias_categories.append(i[x]['alias'])
+
+    for i in sorted(alias_categories):
+        print i
+
+add_categories_to_db()
+
+
+# add_categories_to_db()
 
 def add_to_db():
     pass
-
 
 # call this function only once. Comment Out after running once.
 # to_text_file()
